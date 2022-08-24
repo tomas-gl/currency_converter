50f8caa58d9f9109fa8ce53d0b57984d43578737
tgavrel <tomas.gavrel@hotmail.fr>
2022-08-24 19:43:53 +0200

    Updated display of currencies and conversions in create form :art:

--- admin/src/components/AddCurrencyPair.vue
+++ admin/src/components/AddCurrencyPair.vue
@@ -6,39 +6,41 @@
 
                     <div class="mb-3 col-12 col-md-6">
                         <label for="exampleFormControlSelect1">Première devise</label>
-                            <select class="form-control" v-model="first_currency_id">
-                                <option v-for="currency in currencies" :key="currency.id">{{ currency.iso_code }}</option>
+                            <select class="form-control" v-model="first_currency_id" @change="getFirstSelectedCurrency($event)">
+                                <option v-for="currency in currencies" :key="currency.id" :value="currency.id">{{ currency.iso_code }}</option>
                             </select>
                     </div>
                     <div class="mb-3 col-12 col-md-6">
                         <label for="exampleFormControlSelect1">Seconde devise</label>
-                            <select class="form-control" v-model="second_currency_id">
-                                <option v-for="currency in currencies" :key="currency.id">{{ currency.iso_code }}</option>
+                            <select class="form-control" v-model="second_currency_id" @change="getSecondSelectedCurrency($event)">
+                                <option v-for="currency in currencies" :key="currency.id" :value="currency.id">{{ currency.iso_code }}</option>
                             </select>
                     </div>
 
                     <div class="mb-3 col-12 col-md-5">
-                        <label>Taux de conversion 1</label>
+                        <label>{{ first_selected_currency }}</label>
                         <input disabled class="form-control" placeholder="Taux de conversion" value="1">
                     </div>
                     <div class="mb-3 col-12 col-md-2 text-center">
                         <span class="fs-1">-></span>
                     </div>
                     <div class="mb-3 col-12 col-md-5">
-                        <label>Taux de conversion 2</label>
-                        <input type="number" step=".01" min="0" class="form-control" placeholder="Taux de conversion" v-model="conversion_rate">
+                        <label>{{ second_selected_currency }}</label>
+                        <input type="number" step=".01" min="0" class="form-control" placeholder="Taux de conversion" 
+                                v-model="conversion_rate" 
+                                @change="convertCurrency($event)">
                     </div>
 
                     <div class="mb-3 col-12 col-md-5">
-                        <label>Taux de conversion 2</label>
+                        <label>{{ second_selected_currency }}</label>
                         <input disabled class="form-control" placeholder="Taux de conversion" value="1">
                     </div>
                     <div class="mb-3 col-12 col-md-2 text-center">
                         <span class="fs-1">-></span>
                     </div>
                     <div class="mb-3 col-12 col-md-5">
-                        <label>Taux de conversion 1</label>
-                        <input disabled type="number" step=".01" min="0" class="form-control" placeholder="Taux de conversion">
+                        <label>{{ first_selected_currency }}</label>
+                        <input disabled type="number" step=".01" min="0" class="form-control" placeholder="Taux de conversion" :value="convertedCurrency">
                     </div>
 
                     <div class="alert alert-danger" v-if="errors.length">
@@ -59,22 +61,37 @@
 <script>
     import axios from 'axios';
 
-export default {
+    export default {
         name: 'AddCurrencyPair',
         data(){
             return {
+                first_selected_currency: '',
+                second_selected_currency: '',
+                convertedCurrency: '',
                 currencies:Array,
                 currency_pair: {},
                 first_currency_id: '',
                 second_currency_id: '',
                 conversion_rate: '',
-                errors: []
+                errors: [],
             }
         },        
         created(){
             this.getCurrencies();
         },
         methods:{
+            getFirstSelectedCurrency(event){
+                console.log(event.target.options[event.target.selectedIndex].text);
+                this.first_selected_currency = event.target.options[event.target.selectedIndex].text;
+            },
+            getSecondSelectedCurrency(event){
+                console.log(event.target.options[event.target.selectedIndex].text);
+                this.second_selected_currency = event.target.options[event.target.selectedIndex].text;
+            },
+            convertCurrency(event){
+                console.log(event.target.value);
+                this.convertedCurrency = 1/event.target.value;
+            },
             async getCurrencies(){
                 let url = 'http://127.0.0.1:8000/api/currencies'
                 await axios.get(url).then(response =>{
@@ -101,7 +118,7 @@ export default {
                     formData.append('first_currency_id', this.first_currency_id);
                     formData.append('second_currency_id', this.second_currency_id);
                     formData.append('conversion_rate', this.conversion_rate);
-                    // console.log(formData);
+                    console.log(formData);
                     let url = 'http://127.0.0.1:8000/api/save_currency_pair';
                     await axios.post(url, formData).then((response) =>{
                         console.log(response);
@@ -112,5 +129,5 @@ export default {
                 }
             }
         }
-}
+    }
 </script>
